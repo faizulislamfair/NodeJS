aa580ea4099b5d176267f0852ac9ef2a2833ab89
S.M. Faizul Islam Fair <faizulislamfair@gmail.com>
2021-01-11 21:53:14 +0600

    Runtime Environment

---
+++ app.js
@@ -0,0 +1,8 @@
+
+function sayHello(name) {
+    console.log('Hi ' + name);
+}
+
+sayHello('Fair');
+
+// var portfolio = require('./portfolio.js');
---
+++ portfolio.js
@@ -0,0 +1,9 @@
+
+var url = 'http://myportfolio.io/portfolio';
+
+function portfolio(message) {
+    // Send an HTTP request
+}
+
+module.exports.portfolio = portfolio;
+module.exports.url = url;
