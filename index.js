f1f85a8c82aab178bd27a04f29928d0175e7e65c
S.M. Faizul Islam Fair <faizulislamfair@gmail.com>
2021-01-17 19:36:18 +0600

    I'm not a fan of my step-father

--- index.js
+++ index.js
@@ -49,10 +49,35 @@
 
 // school.startPeriod();
 
-const fs = request('fs');
+// const fs = request('fs');
 
-const ourReadStream = fs.createReadStream(`${__dirname}/bigdata.txt`, 'utf8');
+// const ourReadStream = fs.createReadStream(`${__dirname}/bigdata.txt`, 'utf8');
 
-ourReadStream.on('data', (data) => {
-    console.log(data);
+// ourReadStream.on('data', (data) => {
+//     console.log(data);
+// });
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/') {
+        res.write('<html><head><title>Form</title></head>');
+        res.write(
+            '<body><form method="post" action="/process"><input name="message" /></form></body>'
+            );
+        res.end();
+    } else if(req.url === '/process' && req.method === 'POST'){
+        req.on('data', (chunk) => {
+            console.log(chunk.toString());
+        }); 
+        res.write('Thank you for submitting');
+        res.end();
+    } else {
+        res.write('System Error');
+        res.end();
+    }
 });
+
+server.listen(3000);
+
+console.log('listening on port 3000');
