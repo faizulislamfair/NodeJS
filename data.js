577318e84dc787739cc22d7997993eb8ac228b92
S.M. Faizul Islam Fair <faizulislamfair@gmail.com>
2021-02-21 22:03:18 +0600

    Template Engine

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ var server = http.createServer(function(req, res){
     console.log('request was made: ' + req.url);
     if(req.url === '/home' || req.url === '/' ){
         res.writeHead(200, {'Content-Type': 'text/html'});
-        fs.createReadStream(__dirname + 'index.html').pipe(res);
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
     } else if(req.url === '/contact'){
         res.writeHead(200, {'Content-Type': 'text/html'});
         fs.createReadStream(__dirname + 'contact.html').pipe(res);
