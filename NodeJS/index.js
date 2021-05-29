c972e412cc745f52e3a4daf886e7df81c368edf6
S.M. Faizul Islam Fair <faizulislamfair@gmail.com>
2021-05-29 21:56:32 +0600

    modules

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,7 +1,22 @@
 //console.log("Node App")
 
-const a = 5
-const b = 10
+// const a = 5
+// const b = 10
 //console.log(a,b)
 
-console.log("a is equal to: %s and b is equal to: %s",a,b)
+// console.log("a is equal to: %s and b is equal to: %s",a,b)
+
+// Importing modules
+
+/**      Module       */
+
+// const os = require('os')
+
+// console.log(os.type())
+
+const car = {
+    brand: "BMW",
+    model: "Z4"
+}
+
+module.exports = car;
--- NodeJS/tempCodeRunnerFile.js
+++ NodeJS/tempCodeRunnerFile.js
@@ -1,7 +1,6 @@
-//console.log("Node App")
+const car = {
+    brand: "BMW",
+    model: "Z4"
+}
 
-const a = 5
-const b = 10
-//console.log(a,b)
-
-console.log("a is equal to: %s and b is equal to: %s",a,b)
+module.exports = car;
